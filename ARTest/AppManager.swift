355ba0010fff484f8967442d90e90b6d93417901
WMichaelsen <wimi.pch@procivitas.se>
2017-07-05 08:34:57 +0200

    Made AR Stuff appear only if user is within 30m of sign (dummy locations)

--- ARTest/AppManager.swift
+++ ARTest/AppManager.swift
@@ -7,3 +7,90 @@
 //
 
 import Foundation
+import CoreLocation
+import ARKit
+
+protocol AppManagerDelegate {
+    func addArScene(scene: SCNScene)
+}
+
+class AppManager: LocationManagerDelegate {
+    
+    //MARK: - Properties
+    
+    class var sharedInstance: AppManager {
+        
+        struct Singleton {
+            static let instance = AppManager()
+        }
+        
+        return Singleton.instance
+    }
+    
+    var currentUserLocation: [Double]?
+    var delegate: AppManagerDelegate?
+    
+    
+    /* Location of signs */
+    var signLocations: NSDictionary?
+    
+    //MARK: - Init
+    init() {
+        LocationManager.sharedInstance.delegate = self
+        
+        /* Read and store locations of poet signs */
+        if let path = Bundle.main.path(forResource: "locations", ofType: "plist") {
+            signLocations = NSDictionary(contentsOfFile: path)
+        }
+    }
+    
+    //MARK: - Sign Methods
+    
+    func enteredSignArea() {
+        print("\n\nENTERED REGION\n\n")
+        displayAR()
+    }
+    
+    //MARK: - AR Methods
+    
+    func displayAR() {
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        delegate?.addArScene(scene: scene)
+    }
+    
+    
+    //MARK: - Location Methods
+    
+    func startUpdatingLocation() {
+        LocationManager.sharedInstance.requestPermissionAndStartUpdateLocation()
+    }
+    
+    func stopUpdatingLocation() {
+        LocationManager.sharedInstance.stopUpdating()
+    }
+    
+    //MARK: - LocationManagerDelegate
+    
+    func locationChanged(_ coordinate: CLLocationCoordinate2D?) {
+        if coordinate != nil {
+            
+            /* Go through each sign location and check if user is within 5 meters */
+            for location in (signLocations?.value(forKey: "locs") as? [[Double]])! {
+                let loc1 = CLLocation(latitude: location[0], longitude: location[1])
+                let loc2 = CLLocation(latitude: coordinate!.latitude, longitude: coordinate!.longitude)
+                
+                if loc1.distance(from: loc2) < 30 {
+                    self.enteredSignArea()
+                }
+            }
+            
+            let coordinates: [Double] = [coordinate!.latitude, coordinate!.longitude]
+            
+            /* Update local location variable */
+            self.currentUserLocation = coordinates
+            
+        } else {
+            print("coordinate is nil (UserManager.swift)")
+        }
+    }
+}
--- ARTest/ViewController.swift
+++ ARTest/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, AppManagerDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -20,21 +20,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+//
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecog:)))
+//        view.addGestureRecognizer(tapGesture)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        /* Start checking user location */
+        AppManager.sharedInstance.startUpdatingLocation()
+        AppManager.sharedInstance.delegate = self
     }
+//
+//    @objc
+//    func handleTap(gestureRecog: UITapGestureRecognizer) {
+//        guard let currentFrame = sceneView.session.currentFrame else {
+//            return
+//        }
+//
+//        let imagePlane = SCNPlane(width: sceneView.bounds.width / 6000, height: sceneView.bounds.height / 6000)
+//        imagePlane.firstMaterial?.diffuse.contents = sceneView.snapshot()
+//        imagePlane.firstMaterial?.lightingModel = .constant
+//
+//        let planeNode = SCNNode(geometry: imagePlane)
+//        sceneView.scene.rootNode.addChildNode(planeNode)
+//
+//        var translation = matrix_identity_float4x4
+//        translation.columns.3.z = -1.5
+//        translation.columns.3.x = -1.5
+//        planeNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+//    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingSessionConfiguration()
+        
+        configuration.planeDetection = .horizontal
         
         // Run the view's session
         sceneView.session.run(configuration)
@@ -43,38 +63,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
+    }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         
+    }
+    
+    //MARK: - AppManagerDelegate
+    func addArScene(scene: SCNScene) {
+        sceneView.scene = scene
     }
 }
